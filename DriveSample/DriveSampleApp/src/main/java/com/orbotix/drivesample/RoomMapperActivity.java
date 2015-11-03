880932d9245feb54b2545180baff4f37b214c7fc
Josh Sanz <Joshua.Sanz@ll.mit.edu>
2015-11-02 22:15:42 -0500

    Added Map Room button and empty RoomMapper activity

---
+++ DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/RoomMapperActivity.java
@@ -0,0 +1,13 @@
+package com.orbotix.drivesample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RoomMapperActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_mapper);
+    }
+}
